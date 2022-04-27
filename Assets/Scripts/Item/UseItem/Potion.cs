d738c4efddf9c96c838c9465eea80d5393b7ccd2
Hyeonjun <wns8363@gmail.com>
2022-04-28 04:49:19 +0900

    퀘스트 시스템

--- Assets/Scripts/Item/UseItem/Potion.cs
+++ Assets/Scripts/Item/UseItem/Potion.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Potion", menuName = "Data/Potion")]
 public class Potion : ItemData
 {
+    // public Player player;
     private void Start() {
         code = 0;
         maxCount = 99;
@@ -12,6 +13,7 @@ public class Potion : ItemData
     public override void Use()
     {
         Used();
+        // player.curHp = player.curHp + 10;
         // 플레이어 체력 회복
     }
 }
